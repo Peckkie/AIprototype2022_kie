69b41b4ead46a82018fd1ac9ae1cbd7171cd1075
gigi0107 <w_yupaporn@kkumail.com>
2022-01-11 09:47:44 +0700

    update

--- python_script_101.py
+++ python_script_101.py
@@ -1,5 +1,34 @@
+import argparse #สำหรับ รับ input
+import subprocess #สำหรับ iyo terminal command
+
+import flask  #สำหรับ ทำ web app และ web service
+
 def print_orther():
     print('something else')
     
 if __name__ == "__main__":  #main function
-    print('test main function')
+    parser = argparse.ArgumentParser(description='test program to learn about a  ')
+    parser.add_argument(
+        'M',
+        type = int,
+        help = 'value of x')
+
+    parser.add_argument(
+        '--x',
+        type = int,
+        help = 'value of x')
+
+    parser.add_argument(
+        '--yval', #--yval ตัวใหญ่ เล็ก ไม่มีผล
+        type = int,
+        default = 3,
+        help = 'value of y')
+    args = parser.parse_args()
+    x = args.x
+    y = args.yval
+    print(f'M = {args.M}')
+    print(f'calculate{x} x {y} = {x*y}')
+
+
+    # print('test main function')
+    # print_orther()
